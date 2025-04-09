1cb74389d4dcdb132d841d5e8e2a0b31ce8e436e
GinaKoul <129206273+GinaKoul@users.noreply.github.com>
2025-04-09 21:18:41 +0300

    Create user grid

--- src/js/main.js
+++ src/js/main.js
@@ -5,4 +5,4 @@ import { RenderGameboard } from "./render-gameboard.js";
 const player1 = Player("gina");
 const player2 = Player("demi");
 
-RenderGameboard(player1);
+RenderGameboard(player1, player2);
--- src/js/render-gameboard.js
+++ src/js/render-gameboard.js
@@ -1,40 +1,55 @@
-export const RenderGameboard = function (player) {
+export const RenderGameboard = function (player, opponent) {
+  const name = player.getName();
+  const gameboard = player.getGameboard();
+  const opponentGameboard = opponent.getGameboard();
   const columns = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 
+  const addBoats = () => {
+    gameboard.getPrimaryGrid().forEach((value, key) => {
+      document.getElementById(key).classList.add("occupied");
+    });
+  };
+
+  const handleAttack = (event) => {
+    opponentGameboard.receiveAttack(event.target.getAttribute("id"));
+  };
+
   const renderGrid = (gridName) => {
-    const gridOuter = document.createElement("div");
-    gridOuter.setAttribute("id", gridName);
-    gridOuter.classList.add("grid");
+    const grid = document.createElement("div");
+    grid.setAttribute("id", gridName);
+    grid.classList.add("grid");
 
     const columnHeaders = document.createElement("div");
     columnHeaders.classList.add("column-titles");
 
     const rowHeaders = document.createElement("div");
-    rowHeaders.classList.add("column-titles");
+    rowHeaders.classList.add("row-titles");
 
-    columns.forEach((col, index) => {
-      let gridCol = document.createElement("div");
-      gridCol.classList.add("grid-col");
-
+    for (let index = 1; index <= 10; index++) {
       const columnHeader = document.createElement("div");
-      columnHeader.textContent = col;
+      columnHeader.textContent = columns[index - 1];
       columnHeader.classList.add("grid-header");
       columnHeaders.appendChild(columnHeader);
 
       const rowHeader = document.createElement("div");
-      rowHeader.textContent = index + 1;
+      rowHeader.textContent = index;
       rowHeader.classList.add("grid-header");
       rowHeaders.appendChild(rowHeader);
 
-      for (let index = 1; index <= 10; index++) {
+      columns.forEach((col) => {
         let gridBox = document.createElement("div");
         gridBox.setAttribute("id", `${col}${index}`);
         gridBox.classList.add("grid-box");
-        gridCol.appendChild(gridBox);
-      }
-      gridOuter.appendChild(gridCol);
-    });
-    gridOuter.append(columnHeaders, rowHeaders);
+        if (gridName === "secondary") {
+          gridBox.addEventListener("click", handleAttack);
+        }
+        grid.appendChild(gridBox);
+      });
+    }
+
+    const gridOuter = document.createElement("div");
+    gridOuter.classList.add("grid-outer");
+    gridOuter.append(columnHeaders, rowHeaders, grid);
     return gridOuter;
   };
 
@@ -42,6 +57,14 @@ export const RenderGameboard = function (player) {
     const mainContent = document.querySelector("#content");
     mainContent.appendChild(renderGrid("primary"));
     mainContent.appendChild(renderGrid("secondary"));
+
+    gameboard.placeShip("carrier", ["A1", "A2", "A3", "A4", "A5"]);
+    gameboard.placeShip("battleship", ["B1", "B2", "B3", "B4"]);
+    gameboard.placeShip("destroyer", ["C1", "C2", "C3"]);
+    gameboard.placeShip("submarine", ["D1", "D2", "D3"]);
+    gameboard.placeShip("patrolBoat", ["E1", "E2"]);
+
+    addBoats();
   };
 
   init();
